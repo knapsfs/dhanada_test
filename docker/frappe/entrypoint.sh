8b1277b438b505184c3218ea3dc6a218c5bf5b01
Satyam Raj <smritisoni@DODO-20.local>
2026-08-10 14:24:04 +0530

    updated the entrypoint.sh under frappe, closing version V4.0.2

--- docker/frappe/entrypoint.sh
+++ docker/frappe/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-# Wait for db to be ready (a simple wait could also be handled by docker-compose depends_on, but this is safer)
+export PYTHONPATH="/home/frappe/frappe-bench/apps:${PYTHONPATH}"
+
 if [ -f "/docker/scripts/wait-for-db.sh" ]; then
     /docker/scripts/wait-for-db.sh
 fi
